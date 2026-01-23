484595e17fa9e4725b62a50bfddcaca8990519f9
Adams Miracle <miracle.adams@amalitech.com>
2026-01-23 12:52:39 +0000

    Initial commit

---
+++ src/main/java/com/amalitech/smartecommerce/SmartecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.amalitech.smartecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartecommerceApplication.class, args);
+	}
+
+}
